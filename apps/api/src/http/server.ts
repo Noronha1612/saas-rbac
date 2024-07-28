e9b991c3a4966f767d20bfbe5bcba15fc9320418
Gabriel Noronha <inc.691@gmail.com>
2024-07-28 00:03:01 -0300

    chore: create swagger structure

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,7 +1,9 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -10,6 +12,22 @@ import {
 import { authModule } from './routes/auth/module'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Saas RBAC',
+      description: 'Full stack saas rbac APP',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
