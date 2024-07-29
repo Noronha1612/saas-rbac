46b716d928a59cde3fdcfa7a7fb56a8a57dcf37e
Gabriel Noronha <inc.691@gmail.com>
2024-07-29 18:53:14 -0300

    feat: include authenticate with credentials controller

--- apps/api/src/http/routes/auth/module.ts
+++ apps/api/src/http/routes/auth/module.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticateWithPassword } from './authenticate-with-password'
 import { createAccount } from './create-account'
 
 export async function authModule(app: FastifyInstance) {
   app.register(createAccount)
+  app.register(authenticateWithPassword)
 }
