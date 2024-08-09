f60076f5554c11215cfddd21b084360a24e1de14
Gabriel Noronha <inc.691@gmail.com>
2024-08-09 13:08:22 -0300

    feat: implement update and shutdown organization controllers

--- apps/api/src/http/routes/orgs/module.ts
+++ apps/api/src/http/routes/orgs/module.ts
@@ -4,10 +4,14 @@ import { createOrganization } from './create-organization'
 import { getMembership } from './get-membership'
 import { getOrganization } from './get-organization'
 import { getOrganizations } from './get-organizations'
+import { shutdownOrganization } from './shutdown-organization'
+import { updateOrganization } from './update-organization'
 
 export async function organizationsModule(app: FastifyInstance) {
   app.register(getOrganizations)
   app.register(getOrganization)
   app.register(getMembership)
   app.register(createOrganization)
+  app.register(updateOrganization)
+  app.register(shutdownOrganization)
 }
