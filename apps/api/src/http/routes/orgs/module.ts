950280b3fff88a6894da91d6011409a43d303a4c
Gabriel Noronha <inc.691@gmail.com>
2024-08-09 12:14:24 -0300

    feat: implement get organization details and fetch users organization controllers

--- apps/api/src/http/routes/orgs/module.ts
+++ apps/api/src/http/routes/orgs/module.ts
@@ -2,8 +2,12 @@ import { FastifyInstance } from 'fastify'
 
 import { createOrganization } from './create-organization'
 import { getMembership } from './get-membership'
+import { getOrganization } from './get-organization'
+import { getOrganizations } from './get-organizations'
 
 export async function organizationsModule(app: FastifyInstance) {
+  app.register(getOrganizations)
+  app.register(getOrganization)
   app.register(getMembership)
   app.register(createOrganization)
 }
