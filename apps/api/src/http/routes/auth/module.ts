b956aede954b9591dd9c7177076e2a7906f281a4
Gabriel Noronha <inc.691@gmail.com>
2024-08-08 21:26:34 -0300

    feat: implement request password recover controller

--- apps/api/src/http/routes/auth/module.ts
+++ apps/api/src/http/routes/auth/module.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from 'fastify'
 import { authenticateWithPassword } from './authenticate-with-password'
 import { createAccount } from './create-account'
 import { getProfile } from './get-profile'
+import { requestPasswordRecover } from './request-password-recover'
 
 export async function authModule(app: FastifyInstance) {
   app.register(createAccount)
   app.register(authenticateWithPassword)
   app.register(getProfile)
+  app.register(requestPasswordRecover)
 }
---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/passwords/recover',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Authenticate with email and password',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (req, rep) => {
+      const { email } = req.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        // We don't want people to know if user really exists
+        return rep.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // Send e-mail with password recover link
+      console.log('Recover password token: ', code)
+
+      return rep.status(201).send()
+    },
+  )
+}
