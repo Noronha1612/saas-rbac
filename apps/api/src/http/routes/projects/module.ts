8d72c0428fbdd5114ee6a5fadd5b2beaa7824ad5
Gabriel Noronha <inc.691@gmail.com>
2024-08-11 14:45:32 -0300

    feat: implement update project controller

--- apps/api/src/http/routes/projects/module.ts
+++ apps/api/src/http/routes/projects/module.ts
@@ -4,10 +4,12 @@ import { createProject } from './create-project'
 import { deleteProject } from './delete-project'
 import { getProject } from './get-project'
 import { getProjects } from './get-projects'
+import { updateProject } from './update-project'
 
 export async function projectsModule(app: FastifyInstance) {
   app.register(getProject)
   app.register(getProjects)
   app.register(createProject)
+  app.register(updateProject)
   app.register(deleteProject)
 }
