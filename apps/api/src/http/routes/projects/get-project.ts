536619977f41060eab2165fc16abc4a642687933
Gabriel Noronha <inc.691@gmail.com>
2024-08-11 14:35:08 -0300

    feat: implement get project controller

---
+++ apps/api/src/http/routes/projects/get-project.ts
@@ -0,0 +1,94 @@
+import { projectSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/getUserPermissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:orgSlug/projects/:projectSlug',
+      {
+        schema: {
+          tags: ['Projects'],
+          summary: 'Get a project details',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            orgSlug: z.string(),
+            projectSlug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              project: z.object({
+                id: z.string().uuid(),
+                name: z.string(),
+                slug: z.string(),
+                description: z.string().nullable(),
+                ownerId: z.string().uuid(),
+                avatarUrl: z.string().url().nullable(),
+                organizationId: z.string().uuid(),
+                owner: z.object({
+                  id: z.string().uuid(),
+                  name: z.string().nullable(),
+                  avatarUrl: z.string().url().nullable(),
+                }),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { orgSlug, projectSlug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(orgSlug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        const project = await prisma.project.findUnique({
+          select: {
+            id: true,
+            name: true,
+            slug: true,
+            description: true,
+            ownerId: true,
+            avatarUrl: true,
+            organizationId: true,
+            owner: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+          },
+          where: {
+            slug: projectSlug,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError('Project not found.')
+        }
+
+        const authProject = projectSchema.parse(project)
+
+        if (cannot('get', authProject)) {
+          throw new UnauthorizedError('You are not allowed to see this project')
+        }
+
+        return reply.status(200).send({
+          project,
+        })
+      },
+    )
+}
--- apps/api/src/http/routes/projects/module.ts
+++ apps/api/src/http/routes/projects/module.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify'
 
 import { createProject } from './create-project'
 import { deleteProject } from './delete-project'
+import { getProject } from './get-project'
 
 export async function projectsModule(app: FastifyInstance) {
+  app.register(getProject)
   app.register(createProject)
   app.register(deleteProject)
 }
