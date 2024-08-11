e8feba6962b14d4be5b7ac4be87e732b5e0d0dac
Gabriel Noronha <inc.691@gmail.com>
2024-08-11 13:49:39 -0300

    feat: implement delete project controller

--- apps/api/src/http/routes/projects/module.ts
+++ apps/api/src/http/routes/projects/module.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { createProject } from './create-project'
+import { deleteProject } from './delete-project'
 
 export async function projectsModule(app: FastifyInstance) {
   app.register(createProject)
+  app.register(deleteProject)
 }
