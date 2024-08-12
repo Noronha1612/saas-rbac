0ab0fdc07b83cffa437374e537526eded5d8ef79
Gabriel Noronha <inc.691@gmail.com>
2024-08-12 10:43:41 -0300

    feat: implement reject invite controller

--- apps/api/src/http/routes/invites/module.ts
+++ apps/api/src/http/routes/invites/module.ts
@@ -4,6 +4,7 @@ import { acceptInvite } from './accept-invite'
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
 import { getInvites } from './get-invites'
+import { rejectInvite } from './reject-invite'
 
 export async function invitesModule(app: FastifyInstance) {
   app.register(getInvite)
@@ -11,4 +12,5 @@ export async function invitesModule(app: FastifyInstance) {
   app.register(createInvite)
 
   app.register(acceptInvite)
+  app.register(rejectInvite)
 }
