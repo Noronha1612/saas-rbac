f3ac41badf7121012bd8b31247558c02b6bee064
Gabriel Noronha <inc.691@gmail.com>
2024-08-11 21:21:31 -0300

    feat: implement update member controller

--- apps/api/src/http/routes/members/module.ts
+++ apps/api/src/http/routes/members/module.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { getMembers } from './get-members'
+import { updateMember } from './update-member'
 
 export async function membersModule(app: FastifyInstance) {
   app.register(getMembers)
+  app.register(updateMember)
 }
