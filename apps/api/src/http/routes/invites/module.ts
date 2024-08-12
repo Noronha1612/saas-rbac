12935f0462b7feefcfb0761a710704b7a2125538
Gabriel Noronha <inc.691@gmail.com>
2024-08-12 09:21:19 -0300

    feat: implement get invite controller

--- apps/api/src/http/routes/invites/module.ts
+++ apps/api/src/http/routes/invites/module.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
 import { createInvite } from './create-invite'
+import { getInvite } from './get-invite'
 
 export async function invitesModule(app: FastifyInstance) {
+  app.register(getInvite)
   app.register(createInvite)
 }
