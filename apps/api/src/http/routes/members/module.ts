c63f5f4427c4388e03cc6b9839f876b181fe2ddb
Gabriel Noronha <inc.691@gmail.com>
2024-08-11 21:23:29 -0300

    feat: implement remove member controller

--- apps/api/src/http/routes/members/module.ts
+++ apps/api/src/http/routes/members/module.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 
 import { getMembers } from './get-members'
+import { removeMember } from './remove-member'
 import { updateMember } from './update-member'
 
 export async function membersModule(app: FastifyInstance) {
   app.register(getMembers)
   app.register(updateMember)
+  app.register(removeMember)
 }
---
+++ apps/api/src/http/routes/members/remove-member.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/getUserPermissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function removeMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:orgSlug/members/:memberId',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Remove a member from a organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            orgSlug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { orgSlug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(orgSlug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', 'User')) {
+          throw new UnauthorizedError(
+            'You are not allowed to remove this member from organization.',
+          )
+        }
+
+        await prisma.member.delete({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      },
+    )
+}
