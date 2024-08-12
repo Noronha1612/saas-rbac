623783132e1100d671cc31c095d6e26b03de7a08
Gabriel Noronha <inc.691@gmail.com>
2024-08-12 10:53:10 -0300

    feat: implement revoke invite controller

--- apps/api/src/http/routes/invites/module.ts
+++ apps/api/src/http/routes/invites/module.ts
@@ -4,6 +4,7 @@ import { acceptInvite } from './accept-invite'
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
 import { getInvites } from './get-invites'
+import { getPendingInvites } from './get-pending-invites'
 import { rejectInvite } from './reject-invite'
 import { revokeInvite } from './revoke-invite'
 
@@ -13,6 +14,7 @@ export async function invitesModule(app: FastifyInstance) {
   app.register(createInvite)
   app.register(revokeInvite)
 
+  app.register(getPendingInvites)
   app.register(acceptInvite)
   app.register(rejectInvite)
 }
