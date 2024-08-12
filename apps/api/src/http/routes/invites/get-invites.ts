7f97e499f4e95e452d645fb1a2518c7e63688594
Gabriel Noronha <inc.691@gmail.com>
2024-08-12 09:28:02 -0300

    feat: implement get invites controller

---
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -0,0 +1,84 @@
+import { roleSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/getUserPermissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function getInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:orgSlug/invites',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Create all organization invites',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            orgSlug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  createdAt: z.date(),
+                  role: roleSchema,
+                  email: z.string().email(),
+                  author: z
+                    .object({
+                      name: z.string().nullable(),
+                      id: z.string(),
+                    })
+                    .nullable(),
+                }),
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { orgSlug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(orgSlug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedError(
+            'You are not allowed to get organization invites.',
+          )
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            organizationId: organization.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        return reply.send({ invites })
+      },
+    )
+}
--- apps/api/src/http/routes/invites/module.ts
+++ apps/api/src/http/routes/invites/module.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify'
 
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
+import { getInvites } from './get-invites'
 
 export async function invitesModule(app: FastifyInstance) {
   app.register(getInvite)
+  app.register(getInvites)
   app.register(createInvite)
 }
