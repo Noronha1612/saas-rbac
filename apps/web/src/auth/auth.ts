f011564149976f148603056cbede7c83609203fb
Gabriel Noronha <inc.691@gmail.com>
2024-08-13 18:22:40 -0300

    feat: web get auth data in authenticated routes

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,5 +1,26 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export function isAuthenticated() {
   return !!cookies().get('token')?.value
+}
+
+export async function auth() {
+  const token = cookies().get('token')
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const profile = await getProfile()
+
+    return profile
+  } catch (err) {
+    console.log(err)
+  }
+
+  redirect('/api/auth/sign-out')
 }
