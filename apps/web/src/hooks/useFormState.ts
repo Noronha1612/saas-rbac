dda47b853a611a8168913043241035ecc85ed2e6
Gabriel Noronha <inc.691@gmail.com>
2024-08-13 10:55:24 -0300

    feat: web implement sign-in form action and authenticate user

---
+++ apps/web/src/hooks/useFormState.ts
@@ -0,0 +1,45 @@
+import { FormEvent, useState, useTransition } from 'react'
+
+interface FormState {
+  success: boolean
+  message: string | null
+  errors: Record<string, string[]> | null
+}
+
+interface Options {
+  initialState?: FormState
+  onSuccess?: () => Promise<void> | void
+}
+
+export function useFormState(
+  action: (data: FormData) => Promise<FormState>,
+  { initialState, onSuccess }: Options = {},
+) {
+  const [isPending, startTransition] = useTransition()
+  const [formState, setFormState] = useState<FormState>(
+    initialState ?? {
+      success: false,
+      message: null,
+      errors: null,
+    },
+  )
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const form = event.currentTarget
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const result = await action(data)
+
+      if (result.success && onSuccess) {
+        await onSuccess()
+      }
+
+      setFormState(result)
+    })
+  }
+
+  return [formState, handleSubmit, isPending] as const
+}
